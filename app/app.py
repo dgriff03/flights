b06286ff871d5fad686676ef9e44a92749f3dce5
dgriff03 <daniel.griffin@tufts.edu>
2016-06-07 23:36:33 -0700

    initial data polling logic with data simulation

--- app/app.py
+++ app/app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template, g
+from flask import Flask, render_template, g, request
+
+from datetime import date, timedelta
+import json
 
 app = Flask(__name__)
 
@@ -20,26 +23,68 @@ def omar():
 @app.route('/daniel')
 def daniel():
     g.path = '/daniel'
-    friends = [{'name': 'A', 'airport': 'BOS'},
-               {'name': 'B', 'airport': 'JFK'},
-               {'name': 'C', 'airport': 'LAX'},
-               {'name': 'D', 'airport': 'ORD'},
-               {'name': 'E', 'airport': 'BOS'}]
+    friends = [{'name': 'Keven', 'airport': 'BOS'},
+               {'name': 'Matt', 'airport': 'JFK'},
+               {'name': 'Mark', 'airport': 'LAX'},
+               {'name': 'Stephen', 'airport': 'DCA'},
+               {'name': 'Nick', 'airport': 'DCA'}]
     return flight_grid(friends)
 
 def flight_grid(friends):
     airports = list(set(map(lambda x: x['airport'], friends)))
-    return render_template('flight_grid.html', friends=friends, airports=airports)
+    date_pairs = []
+    def date_formater(d):
+      return d.strftime("%b %d")
 
-@app.route('/flights')
+    return render_template('flight_grid.html',
+      friends=friends, 
+      airports=airports,
+      date_pairs=build_date_pairs(date_formater))
+
+@app.route('/flights.json')
 def flights():
-    omars_fundtion()
-    return render_template('home.html') 
+    origin = request.args.get('origin', '')
+    destination = request.args.get('destination', '')
+    offset = request.args.get('offset', '')
+    return json.dumps(flight_info(origin, destination, offset))
 
 
+############################################
+#             Helper Functions             #
+############################################
 
+def flight_info(origin, destination, offset):
+  price_data = {
+    'BOS': '77',
+    'DCA': '152',
+    'ORD': '88',
+    'JFK': '300',
+    'LAX': '143',
+  }
+  price = int(price_data.get(destination, '204'))
+  price -= int(offset) * 20
+  return {'price': str(price), 'link':  'http://google.com', 'airline': 'AA'}
 
+def build_date_pairs(date_formater):
+  pairs = []
+  friday = get_closest_friday()
+  for i in range(3):
+    friday_offset = timedelta(7 * i)
+    sunday_offset = timedelta(2 + (7 * i))
+    pairs.append(
+      (date_formater(friday + friday_offset),
+       date_formater(friday + sunday_offset))
+    ) 
+  return pairs
 
+def get_closest_friday():
+  today = date.today()
+  day_of_week  = today.weekday()
+  FRIDAY = 4
+  if day_of_week < FRIDAY:
+    day_of_week += 7
+  delta = day_of_week - FRIDAY
+  return today + timedelta(days=delta)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- app/static/flight_grid.js
+++ app/static/flight_grid.js
@@ -1,5 +1,36 @@
+ORIGIN = 'SFO';
+
 $( document ).ready(function() {
-
-
-
+    var numDates = $('.date-headers').length;
+    var airports = Array.prototype.map.call($('#airport-codes span'),
+                                            function(node) {return node.innerHTML;});
+    for (var i = 0; i < numDates; ++i) {
+        for (var j = 0; j < airports.length; ++j) {
+            (function (airport, index) {
+                $.get( 'flights.json',
+                    {origin: ORIGIN,
+                     destination: airport,
+                     offset: index,
+                     }).done(function( data ) {
+                        var jsonData = JSON.parse(data);
+                        var waitTime = Math.round(Math.random() * 10000);
+                        setTimeout(function() {
+                            var queryString = buildQueryString(airport, index);
+                            var cells = $(queryString);
+                            for (var k = 0; k < cells.length; ++k) {
+                                placePriceLink(cells[k], jsonData['price'], jsonData['link'])
+                            }
+                        }, waitTime);
+                     });
+            })( airports[j], i)
+        }
+    }
 });
+
+function placePriceLink(cell, price, link) {
+    cell.innerHTML = '<a href="' + link + ' target="_blank">$' + price + '</a>';
+}
+
+function buildQueryString(aiport, index){
+    return 'tr[data-airport="' + aiport + '"] td[data-index="' + index + '"]';
+}
