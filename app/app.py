e96eb4632eaa0ca3873adb32db32bd63468f34f2
dgriff03 <daniel.griffin@tufts.edu>
2016-06-07 22:05:33 -0700

    initial template, needs styling and functionality

--- app/app.py
+++ app/app.py
@@ -7,6 +7,39 @@ def home():
     g.path = '/'
     return render_template('home.html')
 
+@app.route('/omar')
+def omar():
+    g.path = '/omar'
+    friends = [{'name': 'A', 'airport': 'BOS'},
+               {'name': 'B', 'airport': 'ORD'},
+               {'name': 'C', 'airport': 'LAX'},
+               {'name': 'D', 'airport': 'ORD'},
+               {'name': 'E', 'airport': 'DFW'}]
+    return flight_grid(friends)
+
+@app.route('/daniel')
+def daniel():
+    g.path = '/daniel'
+    friends = [{'name': 'A', 'airport': 'BOS'},
+               {'name': 'B', 'airport': 'JFK'},
+               {'name': 'C', 'airport': 'LAX'},
+               {'name': 'D', 'airport': 'ORD'},
+               {'name': 'E', 'airport': 'BOS'}]
+    return flight_grid(friends)
+
+def flight_grid(friends):
+    airports = list(set(map(lambda x: x['airport'], friends)))
+    return render_template('flight_grid.html', friends=friends, airports=airports)
+
+@app.route('/flights')
+def flights():
+    omars_fundtion()
+    return render_template('home.html') 
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run()
